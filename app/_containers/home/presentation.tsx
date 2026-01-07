f725e0cb0828d96ec9a4967d639fd77917b9a598
elca-hub <maomuhuangying090@gmail.com>
2026-01-07 15:51:30 +0900

    update: root上にwindowを表示

--- app/_containers/home/presentation.tsx
+++ app/_containers/home/presentation.tsx
@@ -1,8 +1,18 @@
 'use client'
 
+import Window from '@/components/ui/window/Window'
+
 /**
  * @package
  */
 export default function HomePresentation() {
-	return <div>hogehoge</div>
+	return (
+		<>
+			<Window title="About Me" onClose={() => { }}>
+				<div>
+					<h1>About Me</h1>
+				</div>
+			</Window>
+		</>
+	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import HomeContainer from '@/app/_containers/home/container'
 
 export default function HomePage() {
   return (
-    <>
+    <div className='flex items-center justify-center max-w-[1200px] mx-auto my-10'>
       <HomeContainer />
-    </>
+    </div>
   )
 }
