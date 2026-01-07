751b9ec535dd532f8d9ea6d2ff6f37e82e1e024a
elca-hub <maomuhuangying090@gmail.com>
2026-01-07 14:52:59 +0900

    chore: ロジックのリファクタリング

--- front/app/components/ui/window/WindowButtons.tsx
+++ front/app/components/ui/window/WindowButtons.tsx
@@ -17,6 +17,7 @@ export default function WindowButtons({
 }) {
   const buttonComponentStyle = "rounded-full border-none cursor-pointer size-6 p-1 hover:scale-90 transition-all duration-300";
   const buttonIconStyle = "text-gray-600 w-full h-full";
+  const buttonDisabledStyle = "opacity-50 cursor-not-allowed pointer-events-none";
 
 
   const [isMaximized, setIsMaximized] = useState(false);
@@ -25,7 +26,6 @@ export default function WindowButtons({
     setIsMaximized(!isMaximized);
     onMaximize();
   }
-
 
   return (
     <div className="inline-flex justify-start items-center gap-3 bg-gray-300 dark:bg-gray-600 p-2 rounded-full">
@@ -36,14 +36,14 @@ export default function WindowButtons({
         <MdClose className={buttonIconStyle} />
       </Button>
       <Button
-        className={`bg-yellow-400 ${buttonComponentStyle} ${isEnabledMinimize ? "" : "opacity-50 cursor-not-allowed pointer-events-none"}`}
+        className={`bg-yellow-400 ${buttonComponentStyle} ${!isEnabledMinimize && buttonDisabledStyle}`}
         onPress={onMinimize}
         isDisabled={!isEnabledMinimize}
       >
         <MdOutlineCloseFullscreen className={buttonIconStyle} />
       </Button>
       <Button
-        className={`bg-green-600 ${buttonComponentStyle} ${isEnabledMaximize ? "" : "opacity-50 cursor-not-allowed pointer-events-none"}`}
+        className={`bg-green-600 ${buttonComponentStyle} ${!isEnabledMaximize && buttonDisabledStyle}`}
         onPress={handleMaximize}
         isDisabled={!isEnabledMaximize}
       >
