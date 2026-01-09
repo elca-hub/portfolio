b103d64cb48205c34d5d42a33788986ee3d70e30
elca-hub <maomuhuangying090@gmail.com>
2026-01-10 02:16:34 +0900

    add: commitlintのruleを追加

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', [
+      'add',
+      'remove',
+      'update',
+      'update',
+      'style'
+    ]]
+  }
 }
