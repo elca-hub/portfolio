2ce4913cc98b0e9cf3b6c9fefcec63abf99ba65f
elca-hub <maomuhuangying090@gmail.com>
2026-01-10 00:08:33 +0900

    chore: webp画像に変換

--- app/_containers/illustrations/content.tsx
+++ app/_containers/illustrations/content.tsx
@@ -11,7 +11,7 @@ function IllustrationItem({
 }) {
   return (
     <section className='flex flex-col items-center justify-center gap-2'>
-      <Image src={`/illustrations/${file}`} alt={title} width={500} height={500} className='w-full h-[30vh] object-cover' />
+      <Image src={`/illustrations/${file}.webp`} alt={title} width={500} height={500} className='w-full h-[30vh] object-cover' />
       <h2 className='text-2xl font-bold'>{title}</h2>
       <p className='text-md text-gray-100 text-center'>{description}</p>
     </section>
@@ -21,22 +21,22 @@ function IllustrationItem({
 export default function IllustrationsContent() {
   const illustrations = [
     {
-      file: '1.jpg',
+      file: '1',
       title: '話を楽しく聞いてくれるお姉さん',
       description: 'ファミレスでご近所さんのお姉さんと仲良く会話するシーン。個人的に手がうまく描けました。',
     },
     {
-      file: '2.jpeg',
+      file: '2',
       title: 'ベー',
       description: '証明写真という、本来は正しく正面から撮るべき場面で抗心を露わにした少女を描きました。',
     },
     {
-      file: '3.png',
+      file: '3',
       title: 'ほえー',
       description: '現アイコンです。手の指と指の間のラインを、本来の線の色とは異なり、やや肌色に近い色に変えてあるのがちょっと工夫した点。',
     },
     {
-      file: '4.jpg',
+      file: '4',
       title: 'はっ、よっ、ほっ！',
       description: '原神より、煙緋を参考に描きました。個人的に一番好きなキャラクターなので、割と丁寧に描いた方です。'
     }
