6030da980faaab0bac865c8f3f188a22c869543f
elca-hub <maomuhuangying090@gmail.com>
2026-01-14 20:33:56 +0900

    add: appsにworksを登録

--- app/_containers/home/container.tsx
+++ app/_containers/home/container.tsx
@@ -3,10 +3,11 @@
 import HomePresentation from '@/app/_containers/home/presentation'
 import HeadContent from '@/components/layout/HeadContent'
 import { AppType } from '@/const/appType'
-import { FaPaintBrush } from 'react-icons/fa'
+import { FaCode, FaPaintBrush } from 'react-icons/fa'
 import { FiUser } from 'react-icons/fi'
 import AboutMeContainer from '../about-me/container'
 import IllustrationsContainer from '../illustrations/container'
+import WorksContainer from '../works/container'
 
 export default async function HomeContainer() {
 	const apps: Record<string, AppType> = {
@@ -21,6 +22,12 @@ export default async function HomeContainer() {
 			content: <IllustrationsContainer />,
 			redirectUrl: '/illustrations',
 			icon: <FaPaintBrush />,
+		},
+		works: {
+			title: 'Works',
+			content: <WorksContainer />,
+			redirectUrl: '/works',
+			icon: <FaCode />,
 		},
 	}
 	const defaultActiveApps = [apps.aboutMe, apps.illustrations]
