921bd162404e0696012cd3c6b83a6384952146f4
elca-hub <maomuhuangying090@gmail.com>
2026-01-15 09:30:15 +0900

    update: sitemapの更新

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -22,5 +22,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			changeFrequency: 'monthly',
 			priority: 0.5,
 		},
+		{
+			url: `${domain}/works`,
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.5,
+		},
 	]
 }
