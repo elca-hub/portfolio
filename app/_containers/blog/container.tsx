1d434502643cea0b4b14aaa9329aced8427d0e2e
elca-hub <maomuhuangying090@gmail.com>
2026-02-07 12:54:12 +0900

    fix: ウインドウで開いてない場合の取得件数を変更

--- app/_containers/blog/container.tsx
+++ app/_containers/blog/container.tsx
@@ -5,7 +5,7 @@ import HeadContent from '@/components/layout/HeadContent'
 import BlogPresentation from './presentation'
 
 export default async function BlogContainer({ isWindow = false }: { isWindow?: boolean }) {
-	const blogs = await fetchBlogs(isWindow ? 10 : 3)
+	const blogs = await fetchBlogs(isWindow ? 10 : 4)
 
 	return (
 		<>
